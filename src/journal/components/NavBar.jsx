ebf6bc40f6bfe8934f36eac95b521b57314f917b
IsraelMerlyn <merlyn142a@gmail.com>
2023-08-10 15:09:38 -0600

    tasks #0.1 agregando Modal para validacion

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,47 +1,64 @@
-import { useDispatch } from 'react-redux';
-import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material';
-import { LogoutOutlined, MenuOutlined } from '@mui/icons-material';
-import { startLogout } from '../../store/auth';
-
-
+import { useDispatch } from "react-redux";
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material";
+import { LogoutOutlined, MenuOutlined } from "@mui/icons-material";
+import { startLogout } from "../../store/auth";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
 export const NavBar = ({ drawerWidth = 240 }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const onLogout = () => {
-        dispatch( startLogout() );
-    }
-
+  const onLogout = () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+    dispatch(startLogout());
+    Toast.fire({
+      icon: "success",
+      title: "sesion cerrada con exito",
+    });
+  };
 
   return (
-    <AppBar 
-        position='fixed'
-        sx={{ 
-            width: { sm: `calc(100% - ${ drawerWidth }px)` },
-            ml: { sm: `${ drawerWidth }px` }
-         }}
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
     >
-        <Toolbar>
-            <IconButton
-                color='inherit'
-                edge="start"
-                sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-                <MenuOutlined />
-            </IconButton>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuOutlined />
+        </IconButton>
 
-            <Grid container direction='row' justifyContent='space-between' alignItems='center'>
-                <Typography variant='h6' noWrap component='div'> JournalApp </Typography>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h6" noWrap component="div">
+            {" "}
+            JournalApp{" "}
+          </Typography>
 
-                <IconButton 
-                    color='error'
-                    onClick={ onLogout }
-                >
-                    <LogoutOutlined />
-                </IconButton>
-            </Grid>
-
-        </Toolbar>
+          <IconButton color="error" onClick={onLogout}>
+            <LogoutOutlined />
+          </IconButton>
+        </Grid>
+      </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,139 +1,160 @@
-import { useEffect, useMemo, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useMemo, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { DeleteOutline, SaveOutlined, UploadOutlined } from '@mui/icons-material';
-import { Button, Grid, IconButton, TextField, Typography } from '@mui/material';
-import Swal from 'sweetalert2';
-import 'sweetalert2/dist/sweetalert2.css';
+import {
+  DeleteOutline,
+  SaveOutlined,
+  UploadOutlined,
+} from "@mui/icons-material";
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
 
-import { useForm } from '../../hooks/useForm';
-import { ImageGallery } from '../components'
-import { setActiveNote, startDeletingNote, startSaveNote, startUploadingFiles } from '../../store/journal';
-
+import { useForm } from "../../hooks/useForm";
+import { ImageGallery } from "../components";
+import {
+  setActiveNote,
+  startDeletingNote,
+  startSaveNote,
+  startUploadingFiles,
+} from "../../store/journal";
 
 export const NoteView = () => {
+  const dispatch = useDispatch();
+  const {
+    active: note,
+    messageSaved,
+    isSaving,
+  } = useSelector((state) => state.journal);
 
-    const dispatch = useDispatch();
-    const { active:note, messageSaved, isSaving } = useSelector( state => state.journal );
+  const { body, title, date, onInputChange, formState } = useForm(note);
 
-    const { body, title, date, onInputChange, formState } = useForm( note );
+  const dateString = useMemo(() => {
+    const newDate = new Date(date);
+    return newDate.toUTCString();
+  }, [date]);
 
-    const dateString = useMemo(() => {
-        const newDate = new Date( date );
-        return newDate.toUTCString();
-    }, [date])
+  const fileInputRef = useRef();
 
-    const fileInputRef = useRef();
+  useEffect(() => {
+    dispatch(setActiveNote(formState));
+  }, [formState]);
 
-    useEffect(() => {
-        dispatch( setActiveNote(formState) );
-    }, [formState])
+  useEffect(() => {
+    if (!messageSaved) {
+      Swal.fire({
+        icon: "error",
+        title: "Error de validacion",
+        text: "Debe completar los campos del formulario!",
+      });
+    }
+    return;
+  }, [messageSaved]);
 
-    useEffect(() => {
-      if ( messageSaved.length > 0 ) {
-          Swal.fire('Nota actualizada', messageSaved, 'success');
+  const onSaveNote = () => {
+    dispatch(startSaveNote());
+  };
+
+  const onFileInputChange = ({ target }) => {
+    if (target.files === 0) return;
+    dispatch(startUploadingFiles(target.files));
+  };
+
+  const onDelete = () => {
+    Swal.fire({
+      title: "Estas seguro que deseas elimnar?",
+      text: "el registro se eliminara permanente!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!",
+      cancelButtonText: "cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Eliminado!", "Registro eliminado con exito", "success");
+        dispatch(startDeletingNote());
       }
-    }, [messageSaved])
-    
-    
+    });
+  };
 
-    const onSaveNote = () => {
-        dispatch( startSaveNote() );
-    }
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ mb: 1 }}
+      className="animate__animated animate__fadeIn animate__faster"
+    >
+      <Grid item>
+        <Typography fontSize={39} fontWeight="light">
+          {dateString}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <input
+          type="file"
+          multiple
+          ref={fileInputRef}
+          onChange={onFileInputChange}
+          style={{ display: "none" }}
+        />
 
-    const onFileInputChange = ({ target }) => {
-        if( target.files === 0 ) return;
-        dispatch( startUploadingFiles( target.files ) );
-    }
-
-    const onDelete = () => {
-        Swal.fire('Nota Eliminada ', messageSaved , 'warning');
-        dispatch( startDeletingNote() );
-    }
-
-
-    return (
-        <Grid 
-            container 
-            direction='row' 
-            justifyContent='space-between' 
-            alignItems='center' 
-            sx={{ mb: 1 }}
-            className='animate__animated animate__fadeIn animate__faster'
+        <IconButton
+          color="primary"
+          disabled={isSaving}
+          onClick={() => fileInputRef.current.click()}
         >
-            <Grid item>
-                <Typography fontSize={ 39 } fontWeight='light' >{ dateString }</Typography>
-            </Grid>
-            <Grid item>
+          <UploadOutlined />
+        </IconButton>
 
-                <input 
-                    type="file"
-                    multiple
-                    ref={ fileInputRef }
-                    onChange={ onFileInputChange }
-                    style={{ display: 'none' }}
-                />
+        <Button
+          disabled={isSaving}
+          onClick={onSaveNote}
+          color="primary"
+          sx={{ padding: 2 }}
+        >
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Guardar
+        </Button>
+      </Grid>
 
-                <IconButton
-                    color="primary"
-                    disabled={ isSaving }
-                    onClick={ () => fileInputRef.current.click() }
-                >
-                    <UploadOutlined />
-                </IconButton>
-                
-                <Button 
-                    disabled={ isSaving }
-                    onClick={ onSaveNote }
-                    color="primary" 
-                    sx={{ padding: 2 }}
-                >
-                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-                    Guardar
-                </Button>
-            </Grid>
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          placeholder="Ingrese un título"
+          label="Título"
+          sx={{ border: "none", mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
+        />
 
-            <Grid container>
-                <TextField 
-                    type="text"
-                    variant="filled"
-                    fullWidth
-                    placeholder="Ingrese un título"
-                    label="Título"
-                    sx={{ border: 'none', mb: 1 }}
-                    name="title"
-                    value={ title }
-                    onChange={ onInputChange }
-                />
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="¿Qué sucedió en el día de hoy?"
+          minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
+        />
+      </Grid>
 
-                <TextField 
-                    type="text"
-                    variant="filled"
-                    fullWidth
-                    multiline
-                    placeholder="¿Qué sucedió en el día de hoy?"
-                    minRows={ 5 }
-                    name="body"
-                    value={ body }
-                    onChange={ onInputChange }
-                />
-            </Grid>
+      <Grid container justifyContent="end">
+        <Button onClick={onDelete} sx={{ mt: 2 }} color="error">
+          <DeleteOutline />
+          Borrar
+        </Button>
+      </Grid>
 
-            <Grid container justifyContent='end'>
-                <Button
-                    onClick={ onDelete }
-                    sx={{ mt: 2 }}
-                    color="error"
-                >
-                    <DeleteOutline />
-                    Borrar
-                </Button>
-            </Grid>
-
-
-            {/* Image gallery */}
-            <ImageGallery images={ note.imageUrls } />
-
-        </Grid>
-    )
-}
+      {/* Image gallery */}
+      <ImageGallery images={note.imageUrls} />
+    </Grid>
+  );
+};
